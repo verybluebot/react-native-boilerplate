e556e21a0cefd26fb738ababb93a06e45b6d866c
shulg <verybluebot@gmail.com>
2017-08-01 17:57:35 +0300

    added home screen

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Home from './screens/Home';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Home />
+        );
+    }
+}
---
+++ app/screens/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+const Home = (props) => {
+    return (
+        <View>
+            <Text>
+                this is home
+            </Text>
+        </View>
+    )
+};
+
+export default Home;
